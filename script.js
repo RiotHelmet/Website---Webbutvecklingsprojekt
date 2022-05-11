a1c24a916923f904d6df5a8ddfa3b3bd17c75bd7
RiotHelmet <85107883+RiotHelmet@users.noreply.github.com>
2022-05-11 14:44:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -14,11 +14,17 @@ async function getMovies() {
   <div class="slideText">
   <h2>${resPop.results[0].title}</h2>
   <div class="slideButtons"> 
-  <div class="slidePlay" ><i class="fa-solid fa-circle-play"></i> Play Title </div> 
-  <div class="slideInfo" onclick="moreInfo(this)" > <i class="fa-solid fa-circle-info"></i> More Info </div> 
+  <div class="slidePlay" movieId="${allMovies.indexOf(
+    resPop.results[0]
+  )}" ><i class="fa-solid fa-circle-play"></i> Play Title </div> 
+  <div class="slideInfo" movieId="${allMovies.indexOf(
+    resPop.results[0]
+  )}" onclick="moreInfo()" > <i class="fa-solid fa-circle-info"></i> More Info </div> 
   </div>
   </div>
-  <img src="https://image.tmdb.org/t/p/original${resPop.results[0].backdrop_path}" class="slideFilm">`;
+  <img src="https://image.tmdb.org/t/p/original${
+    resPop.results[0].backdrop_path
+  }" class="slideFilm">`;
 
   resPop.results.forEach((film) => {
     if (resPop.results.indexOf(film) > 9) {
@@ -35,8 +41,9 @@ async function getMovies() {
     <i class="fa-solid fa-circle-play" id="overlayPlay" movieId="${allMovies.indexOf(
       film
     )}" onclick="play(this)"></i>
-
-    <i class="fa-solid fa-circle-info" id="overlayInfo"></i>
+    <i class="fa-solid fa-circle-info" id="overlayInfo" movieId="${allMovies.indexOf(
+      film
+    )}"></i>
     </div>
     </div>
     </div>
@@ -67,3 +74,26 @@ function play(Object) {
 }
 
 getMovies();
+
+function moreInfo() {
+  let infoBackground = document.getElementById("infoBackground");
+  infoBackground.style.display = "flex";
+  document.getElementById("infoContainer").style.animation =
+    "openInfo 0.2s ease-in-out";
+}
+
+function closeInfo() {
+  let infoBackground = document.getElementById("infoBackground");
+  infoBackground.style.display = "none";
+}
+
+function shakeRow() {
+  for (var i = 0 + 5 * currentRow, len = 5 + 5 * currentRow; i < len; i++) {
+    document.getElementById(`${i}`).style.animation = "shake 0.5s ease-in";
+  }
+  setTimeout(function () {
+    for (var i = 0 + 5 * currentRow, len = 5 + 5 * currentRow; i < len; i++) {
+      document.getElementById(`${i}`).style.animation = null;
+    }
+  }, 1000);
+}
